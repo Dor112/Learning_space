b438cd84bf5e7a4c8b6526e9f6407c233c0dcc50
Dor112 <125490773+Dor112@users.noreply.github.com>
2024-11-01 17:18:47 +0300

    Add files via upload

---
+++ RK1_INF.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+#include <vector>
+#include <string>
+
+#include <iomanip>
+
+using namespace std;
+//int main()
+//{
+//
+//	setlocale(LC_ALL, "RU");
+//
+//
+//	cout << "Task 4: " << endl;
+//	cout << "A B C D F" << endl;
+//	for (int A = 0; A < 2; A++)
+//	{
+//		for (int B = 0; B < 2; B++)
+//		{
+//			for (int C = 0; C < 2; C++)
+//			{
+//				for (int D = 0; D < 2; D++)
+//				{
+//							if ((A && !B) || (!C || D))
+//							{
+//								cout << A << "  " << B << "  " << C << "  " << D << "  1" << endl;
+//							}
+//							else
+//							{
+//								cout << A << "  " << B << "  " << C << "  " << D << "  0" << endl;
+//							}
+//						
+//					
+//				}
+//			}
+//		}
+//	}
+//
+//	return 0;
+//
+//}
+//int main()
+//{
+//
+//	setlocale(LC_ALL, "RU");
+//
+//
+//	cout << "Task 6: " << endl;
+//	cout << "A B C F" << endl;
+//	for (int A = 0; A < 2; A++)
+//	{
+//		for (int B = 0; B < 2; B++)
+//		{
+//			for (int C = 0; C < 2; C++)
+//			{
+//					if (!A || (!B && C))
+//					{
+//						cout << A << "  " << B << "  " << C << "  1" << endl;
+//					}
+//					else
+//					{
+//						cout << A << "  " << B << "  " << C  << "  0" << endl;
+//					}
+//			}
+//		}
+//	}
+//
+//	return 0;
+//
+//}
